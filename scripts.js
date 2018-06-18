8634c9e716a0b1b429fff549d3ee577020d0b30e
Tom <tarce@botanichealth.com>
2018-06-18 18:31:56 -0500

    most of the header replicated, needs the nav bar

---
+++ scripts.js
@@ -0,0 +1,31 @@
+//////////////////////
+/// Mobile Header ///
+////////////////////
+
+/// Variables
+var menuIcon = $(".header-menu-icon");
+var searchIcon = $(".header-search-icon");
+var searchIconClose = $(".search-close");
+var mobileMenu = $("nav.header-mobile-menu");
+var searchForm = $("form.header-search");
+
+/// Show Mobile Menu
+$(menuIcon).on("click", function() {
+    if($(searchForm).is(":visible")) {
+        $(searchForm).removeClass("show");
+    }
+    $(mobileMenu).slideToggle(200);
+});
+
+/// Show Search Form
+$(searchIcon).on("click", function() {
+    if($(mobileMenu).is(":visible")) {
+        $(mobileMenu).hide();
+    }
+    $(searchForm).toggleClass("show");
+});
+
+/// Hide Search Form
+$(searchIconClose).on("click", function() {
+    $(searchForm).removeClass("show");
+});
