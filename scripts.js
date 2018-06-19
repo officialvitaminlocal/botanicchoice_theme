cf3399d494521d80612bbab1e89b7e4d7c18ff84
tomarinrc <tomarinrc@gmail.com>
2018-06-18 22:33:07 -0500

    header

--- scripts.js
+++ scripts.js
@@ -10,22 +10,34 @@ var mobileMenu = $("nav.header-mobile-menu");
 var searchForm = $("form.header-search");
 
 /// Show Mobile Menu
-$(menuIcon).on("click", function() {
-    if($(searchForm).is(":visible")) {
+$(menuIcon).on("click", function () {
+    if ($(searchForm).is(":visible")) {
         $(searchForm).removeClass("show");
     }
     $(mobileMenu).slideToggle(200);
 });
 
 /// Show Search Form
-$(searchIcon).on("click", function() {
-    if($(mobileMenu).is(":visible")) {
+$(searchIcon).on("click", function () {
+    if ($(mobileMenu).is(":visible")) {
         $(mobileMenu).hide();
     }
     $(searchForm).toggleClass("show");
 });
 
 /// Hide Search Form
-$(searchIconClose).on("click", function() {
+$(searchIconClose).on("click", function () {
     $(searchForm).removeClass("show");
+});
+
+
+///////////////////////
+/// Desktop Header ///
+/////////////////////
+$(document).on("scroll", function () {
+    if ($(document).scrollTop() > 180) {
+        $("header").addClass("shrink");
+    } else {
+        $("header").removeClass("shrink");
+    }
 });
